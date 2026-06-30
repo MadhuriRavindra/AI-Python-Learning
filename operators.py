27e293523631d7d7a81a95f54c46a355b5d81bbc
Vinay Sathyanarayan <madhuri.ravindra@gmail.com>
2026-06-30 05:21:19 +0200

    moving operators code

---
+++ operators.py
@@ -0,0 +1,27 @@
+# #Add and assign
+# x=10
+# print(x)
+# x+=20
+# print(x)
+
+# x-=4
+# print(x)
+
+# #comparision operators
+# a=10
+# b=20
+# print (a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+# #Logical operators
+# # AND 
+# # OR
+
+attendance = 80
+medical = False
+print(attendance>=75 and medical)
+print(True or False)
