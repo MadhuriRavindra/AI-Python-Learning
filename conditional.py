af3bef110e20079c82aa9b2795ee16c08e0db846
Vinay Sathyanarayan <madhuri.ravindra@gmail.com>
2026-07-01 04:30:02 +0200

    adding conditional statements

---
+++ conditional.py
@@ -0,0 +1,18 @@
+# #if-else
+# age = int(input("Enter your age:"))
+# if age >=18:
+#     print("you are eligeble to vote")
+# else:
+#     print("you are not eligible to vote")
+
+#if-elif
+marks = int(input("Please enter your marks:"))
+if marks>=90:
+    print("Grade A")
+elif marks>=75:
+    print("Grade B")
+elif marks>=50:
+    print("Grade C")
+else:
+    print("Student is not going to next class")
+
--- operators.py
+++ operators.py
@@ -1,25 +1,4 @@
-# #Add and assign
-# x=10
-# print(x)
-# x+=20
-# print(x)
-
-# x-=4
-# print(x)
-
-# #comparision operators
-# a=10
-# b=20
-# print (a==b)
-# print(a!=b)
-# print(a>b)
-# print(a<b)
-# print(a>=b)
-# print(a<=b)
-
-# #Logical operators
-# # AND 
-# # OR
+# jkl;
 
 attendance = 80
 medical = False
